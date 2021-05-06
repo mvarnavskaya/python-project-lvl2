418d9e48897e5dc4305654b219f25db6013b7033
Марина Варнавская <m.varnavskaya@litebox.ru>
2021-05-07 00:06:49 +0400

    run2

---
+++ scripts/gendiff.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import argparse
+from gendiff.generate_diff import generate_diff
+
+
+def createParser():
+    parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', help='set format of output')
+    return parser
+
+
+def main():
+    parser = createParser()
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/generate_diff.py
@@ -0,0 +1,1 @@
+def generate_diff(first_file, second_file):
