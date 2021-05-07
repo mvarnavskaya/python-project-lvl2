6dae2133039c7bc41b28f0b33396b634ade81712
Марина Варнавская <m.varnavskaya@litebox.ru>
2021-05-07 22:55:49 +0400

    run4

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,10 +4,10 @@ from gendiff import generate_diff
 
 
 def test_flat_json():
-    a = json.load(open('./tests/fixtures/diff_file1_file2.json'))
+    a = json.load(open('./fixtures/diff_file1_file2.json'))
     b = generate_diff.get_diff(
-        './tests/fixtures/file1.json',
-        './tests/fixtures/file2.json'
+        './fixtures/file1.json',
+        './fixtures/file2.json'
     )
 
     assert len(a.read()) == len(b)
