559dca56997ebd6bf5cdc5624035862eb5757c2a
Марина Варнавская <m.varnavskaya@litebox.ru>
2021-05-27 17:01:02 +0400

    tests

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,10 +2,18 @@
 import argparse
 
 
+DEFAULT_STYLE = 'stylish'
+
+
 def run():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format_name', help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format_name',
+        default=DEFAULT_STYLE,
+        help="set format of output (default: '{0}')".format(DEFAULT_STYLE)
+    )
     args = parser.parse_args()
     return args
